658987ec1f5d826ae7e028c800c1c64ad686c713
SaroshAhmed22 <SaroshAhmed622@gmail.com>
2021-08-19 00:21:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="">
+    <div className="text-center">
       <h1>
         As salam o alikum from Sarosh Ahmed this is live surge website deployed
         with live changes
