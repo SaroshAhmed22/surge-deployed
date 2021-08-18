436fd71ac56cdde6395a9a290f52dd8dd7770a8e
SaroshAhmed22 <SaroshAhmed622@gmail.com>
2021-08-19 00:10:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="">
-    <h1>As salam o alikum from Sarosh Ahmed</h1>
-     
+      <h1>As salam o alikum from Sarosh Ahmed this is live surge website</h1>
     </div>
   );
 }
